c0377807f883ce897a308cf06131530d604dcf84
LazarJ3v <lazarjevtic202@gmail.com>
2026-08-10 14:17:17 +0200

    feat(auth): Add auth controller

--- Application/scrumflow-backend/src/auth/auth.controller.ts
+++ Application/scrumflow-backend/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, HttpCode } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    logIn(@Body() login: { email: string, password: string}) {
+        const {email, password} = login;
+        return this.authService.loginUser(email, password);
+    }
+}
